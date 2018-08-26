3a012d3bc32f4892cd0ddf1d1d889d7bf53b243f
Charlie Haag <charlottehaag22@gmail.com>
2018-08-25 19:42:57 -0700

    homepage first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+$(window).on('load', function() {
+  setTimeout(function(){
+    $('h1').addClass('show');
+  }, 50);
+    setTimeout(function(){
+      $('#emoji-girl').addClass('isVisible');
+    }, 1500);
+  })
