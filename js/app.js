e43f5ee950741f0bdd949194bc1057775e016e21
Charlie Haag <charlottehaag22@gmail.com>
2019-05-03 17:22:09 -0700

    added transition to about page

--- js/app.js
+++ js/app.js
@@ -3,7 +3,13 @@ $(window).on('load', function() {
   setTimeout(function(){
     $('h1').addClass('show');
   }, 50);
-    setTimeout(function(){
-      $('#emoji-girl').addClass('isVisible');
-    }, 1500);
-  })
+  setTimeout(function(){
+    $('#emoji-girl').addClass('isVisible');
+  }, 1500);
+  setTimeout(function(){
+    $('.about-content').addClass('appear');
+  }, 50);
+  setTimeout(function(){
+    $('nav').addClass('drop');
+  }, 1500);
+})
